b607c081ee68c587559e8dc74e06331767fdbef0
Henry de Valence <hdevalence@hdevalence.ca>
2021-04-13 16:47:59 -0700

    Remove code using deleted protobuf structures.

--- tendermint/src/block/size.rs
+++ tendermint/src/block/size.rs
@@ -1,12 +1,8 @@
 //! Block size parameters
 
-use crate::{Error, Kind};
-use std::convert::{TryFrom, TryInto};
-use tendermint_proto::Protobuf;
 use {
     crate::serializers,
     serde::{Deserialize, Serialize},
-    tendermint_proto::abci::BlockParams as RawSize,
 };
 
 /// Block size parameters
@@ -19,30 +15,4 @@ pub struct Size {
     /// Maximum amount of gas which can be spent on a block
     #[serde(with = "serializers::from_str")]
     pub max_gas: i64,
-}
-
-impl Protobuf<RawSize> for Size {}
-
-impl TryFrom<RawSize> for Size {
-    type Error = Error;
-
-    fn try_from(value: RawSize) -> Result<Self, Self::Error> {
-        Ok(Self {
-            max_bytes: value
-                .max_bytes
-                .try_into()
-                .map_err(|_| Self::Error::from(Kind::IntegerOverflow))?,
-            max_gas: value.max_gas,
-        })
-    }
-}
-
-impl From<Size> for RawSize {
-    fn from(value: Size) -> Self {
-        // Todo: make the struct more robust so this can become infallible.
-        RawSize {
-            max_bytes: value.max_bytes as i64,
-            max_gas: value.max_gas,
-        }
-    }
 }
