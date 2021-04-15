7a068bfd4ff98bc9b9fdfa97a8b6f4238940993f
Henry de Valence <hdevalence@hdevalence.ca>
2021-04-15 14:56:04 -0700

    Add generic Event constructors

--- tendermint/src/abci/event.rs
+++ tendermint/src/abci/event.rs
@@ -19,7 +19,42 @@ pub struct Event {
     pub attributes: Vec<EventAttribute>,
 }
 
+impl Event {
+    /// Construct an event from generic data.
+    ///
+    /// The `From` impls on [`EventAttribute`] and the [`EventAttributeIndexExt`]
+    /// trait allow ergonomic event construction, as in this example:
+    ///
+    /// ```
+    /// use tendermint::abci::event::{Event, EventAttributeIndexExt};
+    ///
+    /// let event = Event::new(
+    ///     "app",
+    ///     vec![
+    ///         ("key1", "value1").index(),
+    ///         ("key2", "value2").index(),
+    ///         ("key3", "value3").no_index(), // will not be indexed
+    ///     ],
+    /// );
+    /// ```
+    // XXX(hdevalence): remove vec! from example after https://github.com/rust-lang/rust/pull/65819
+    pub fn new<K, I>(kind: K, attributes: I) -> Self
+    where
+        K: Into<String>,
+        I: IntoIterator,
+        I::Item: Into<EventAttribute>,
+    {
+        Self {
+            kind: kind.into(),
+            attributes: attributes.into_iter().map(Into::into).collect(),
+        }
+    }
+}
+
 /// A key-value pair describing an [`Event`].
+///
+/// Generic methods are provided for more ergonomic attribute construction, see
+/// [`Event::new`] for details.
 ///
 /// [ABCI documentation](https://docs.tendermint.com/master/spec/abci/abci.html#events)
 #[derive(Clone, PartialEq, Eq, Debug)]
@@ -32,6 +67,56 @@ pub struct EventAttribute {
     ///
     /// **This field is nondeterministic**.
     pub index: bool,
+}
+
+impl<K: Into<String>, V: Into<String>> From<(K, V, bool)> for EventAttribute {
+    fn from((key, value, index): (K, V, bool)) -> Self {
+        EventAttribute {
+            key: key.into(),
+            value: value.into(),
+            index,
+        }
+    }
+}
+
+/// Adds convenience methods to tuples for more ergonomic [`EventAttribute`]
+/// construction.
+///
+/// See [`Event::new`] for details.
+#[allow(missing_docs)]
+pub trait EventAttributeIndexExt: private::Sealed {
+    type Key;
+    type Value;
+
+    /// Indicate that this key/value pair should be indexed by Tendermint.
+    fn index(self) -> (Self::Key, Self::Value, bool);
+    /// Indicate that this key/value pair should not be indexed by Tendermint.
+    fn no_index(self) -> (Self::Key, Self::Value, bool);
+}
+
+impl<K: Into<String>, V: Into<String>> EventAttributeIndexExt for (K, V) {
+    type Key = K;
+    type Value = V;
+    fn index(self) -> (K, V, bool) {
+        let (key, value) = self;
+        (key, value, true)
+    }
+    fn no_index(self) -> (K, V, bool) {
+        let (key, value) = self;
+        (key, value, false)
+    }
+}
+
+mod private {
+    pub trait Sealed {}
+
+    impl<K: Into<String>, V: Into<String>> Sealed for (K, V) {}
+}
+
+impl<K: Into<String>, V: Into<String>> From<(K, V)> for EventAttribute {
+    fn from((key, value): (K, V)) -> Self {
+        (key, value, false).into()
+    }
 }
 
 // =============================================================================
