04e37fc94f6da36261394cb0e478b975d2c5c97f
R2D2 <jacob.turner870@gmail.com>
2021-09-14 16:56:51 +0200

    Generated the proto files and adjust the abci application trait

--- proto/src/tendermint.rs
+++ proto/src/tendermint.rs
@@ -4,22 +4,14 @@ pub mod consensus {
     include!("prost/tendermint.consensus.rs");
 }
 
-pub mod types {
-    include!("prost/tendermint.types.rs");
-}
-
-pub mod mempool {
-    include!("prost/tendermint.mempool.rs");
-}
-
 pub mod rpc {
     pub mod grpc {
         include!("prost/tendermint.rpc.grpc.rs");
     }
 }
 
-pub mod blockchain {
-    include!("prost/tendermint.blockchain.rs");
+pub mod p2p {
+    include!("prost/tendermint.p2p.rs");
 }
 
 pub mod libs {
@@ -28,40 +20,43 @@ pub mod libs {
     }
 }
 
-pub mod state {
-    include!("prost/tendermint.state.rs");
+pub mod crypto {
+    include!("prost/tendermint.crypto.rs");
 }
 
-pub mod version {
-    include!("prost/tendermint.version.rs");
-}
-
-pub mod store {
-    include!("prost/tendermint.store.rs");
-}
-
-pub mod privval {
-    include!("prost/tendermint.privval.rs");
+pub mod mempool {
+    include!("prost/tendermint.mempool.rs");
 }
 
 pub mod statesync {
     include!("prost/tendermint.statesync.rs");
 }
 
-pub mod p2p {
-    include!("prost/tendermint.p2p.rs");
+pub mod types {
+    include!("prost/tendermint.types.rs");
+}
+
+pub mod blocksync {
+    include!("prost/tendermint.blocksync.rs");
+}
+
+pub mod version {
+    include!("prost/tendermint.version.rs");
+}
+
+pub mod state {
+    include!("prost/tendermint.state.rs");
 }
 
 pub mod abci {
-    use crate::prelude::*;
     include!("prost/tendermint.abci.rs");
 }
 
-pub mod crypto {
-    include!("prost/tendermint.crypto.rs");
+pub mod privval {
+    include!("prost/tendermint.privval.rs");
 }
 
 pub mod meta {
     pub const REPOSITORY: &str = "https://github.com/tendermint/tendermint";
-    pub const COMMITISH: &str = "v0.34.9";
+    pub const COMMITISH: &str = "abci++";
 }
