42332fe33bd46db62f5faba9206ed0325289ff6a
satan <jacob.turner870@gmail.com>
2022-10-24 12:20:43 +0200

    [feat]: Compiled newest proto defs

--- proto/src/tendermint.rs
+++ proto/src/tendermint.rs
@@ -1,31 +1,7 @@
 //! Tendermint-proto auto-generated sub-modules for Tendermint
 
-pub mod consensus {
-    include!("prost/tendermint.consensus.rs");
-}
-
-pub mod rpc {
-    pub mod grpc {
-        include!("prost/tendermint.rpc.grpc.rs");
-    }
-}
-
-pub mod p2p {
-    include!("prost/tendermint.p2p.rs");
-}
-
-pub mod libs {
-    pub mod bits {
-        include!("prost/tendermint.libs.bits.rs");
-    }
-}
-
-pub mod crypto {
-    include!("prost/tendermint.crypto.rs");
-}
-
-pub mod mempool {
-    include!("prost/tendermint.mempool.rs");
+pub mod version {
+    include!("prost/tendermint.version.rs");
 }
 
 pub mod statesync {
@@ -36,31 +12,55 @@ pub mod store {
     include!("prost/tendermint.store.rs");
 }
 
-pub mod types {
-    include!("prost/tendermint.types.rs");
+pub mod state {
+    include!("prost/tendermint.state.rs");
+}
+
+pub mod mempool {
+    include!("prost/tendermint.mempool.rs");
+}
+
+pub mod consensus {
+    include!("prost/tendermint.consensus.rs");
 }
 
 pub mod blocksync {
     include!("prost/tendermint.blocksync.rs");
 }
 
-pub mod version {
-    include!("prost/tendermint.version.rs");
-}
-
-pub mod state {
-    include!("prost/tendermint.state.rs");
-}
-
 pub mod abci {
     include!("prost/tendermint.abci.rs");
+}
+
+pub mod types {
+    include!("prost/tendermint.types.rs");
+}
+
+pub mod crypto {
+    include!("prost/tendermint.crypto.rs");
+}
+
+pub mod p2p {
+    include!("prost/tendermint.p2p.rs");
 }
 
 pub mod privval {
     include!("prost/tendermint.privval.rs");
 }
 
+pub mod libs {
+    pub mod bits {
+        include!("prost/tendermint.libs.bits.rs");
+    }
+}
+
+pub mod rpc {
+    pub mod grpc {
+        include!("prost/tendermint.rpc.grpc.rs");
+    }
+}
+
 pub mod meta {
     pub const REPOSITORY: &str = "https://github.com/tendermint/tendermint";
-    pub const COMMITISH: &str = "v0.37.x";
+    pub const COMMITISH: &str = "v0.37.0-rc1";
 }
